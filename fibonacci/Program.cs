2c0149ce6cfe7337dcf43c8de856cc43e5421240
kevileem2 <32266918+kevileem2@users.noreply.github.com>
2017-10-17 15:34:58 +0200

    fibonacci

---
+++ fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give the length of the fibonacci code:");
+            Int64 input = Convert.ToInt64(Console.ReadLine());
+            Int64[] arr = new Int64[input];
+            arr[0] = 0;
+            arr[1] = 1;
+            for (int i = 0; i < input-2; i++){
+                arr[i+2] = arr[i] + arr[i+1];
+            }
+            for(int i = 0; i < arr.Length; i++){
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
