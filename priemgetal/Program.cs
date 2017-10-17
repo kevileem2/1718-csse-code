c1eeda2ac4acc72e3acfb5d22d303e38b4209553
kevileem2 <32266918+kevileem2@users.noreply.github.com>
2017-10-17 15:39:08 +0200

    add opdracht prime numbers

---
+++ priemgetal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace priemgetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number under 100");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= input; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++) 
+                {
+                    if (i % j == 0) 
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime && i > 1)
+                {
+                    Console.WriteLine ( "Prime:" + i );
+                }
+                // isPrime = true;
+            }
+
+        }
+    }
+}
